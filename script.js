7ffde2edf4f2447b6a3ab5669a05922308c755e2
NielzosFilmsSchool <niels.hazelaar@student.rocva.nl>
2020-03-09 14:03:29 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var bg;
+var fg;
+
 function changeColor(){
-    document.querySelector("body").style.backgroundColor = document.querySelector(".color").value;
+    document.querySelector("body").style.backgroundColor = bg;
+    document.querySelector("h1").style.color = fg;
+}
+
+function saveBG(){
+    bg = document.querySelector(".color").value;
+}
+
+function saveFG(){
+    fg = document.querySelector(".color").value;
 }
